358d34840583cd43208fe93b17f10fca093f4987
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:51 +0700

    Initial commit: ModuleGeorgianLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleGeorgianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGeorgianLanguagePack' => 'Georgian Language Pack',
+    'SubHeaderModuleGeorgianLanguagePack' => 'Complete Georgian language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleGeorgianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGeorgianLanguagePack' => 'Языковой пакет - Грузинский',
+    'SubHeaderModuleGeorgianLanguagePack' => 'Комплексная поддержка грузинского языка для системы MikoPBX',
+];
